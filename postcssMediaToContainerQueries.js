06f3fa70702d833cf110722c1331f8ea5f23737b
Jed <vjeudy@protonmail.com>
2024-10-30 12:06:33 +0100

    wip tests

--- postcssMediaToContainerQueries.js
+++ postcssMediaToContainerQueries.js
@@ -12,15 +12,13 @@ const OPERATORS = [
   '<='
 ];
 
-module.exports = ({ debug = false, selector = '' } = {}) => {
+module.exports = ({
+  debug = false, transform = null, selector = ''
+} = {}) => {
   return {
     postcssPlugin: 'postcss-media-to-container-queries',
     Once (root, postcss) {
       root.walkAtRules('media', atRule => {
-        /* console.log('rule', rule.source); */
-        /* console.log('rule.params', rule.params); */
-        console.log('rule', atRule.nodes);
-
         if (
           atRule.params.includes('print') &&
         (!atRule.params.includes('all') || !atRule.params.includes('screen'))
@@ -31,7 +29,10 @@ module.exports = ({ debug = false, selector = '' } = {}) => {
         // Container query
         const containerAtRule = atRule.clone({
           name: 'container',
-          params: convertToContainerQuery(atRule.params)
+          params: convertToContainerQuery(atRule.params, {
+            debug,
+            transform
+          })
             .replaceAll(/(only\s*)?(all|screen|print)(,)?(\s)*(and\s*)?/g, '')
         });
 
@@ -52,90 +53,23 @@ module.exports = ({ debug = false, selector = '' } = {}) => {
         });
 
         root.append(containerAtRule);
-
-        /* rule.walkDecls(decl => { */
-        /*   /* console.log('decl', decl); */
-        /* }); */
       });
     }
   };
 };
 
-function convertToContainerQuery (mediaFeature) {
-  const containerFeature = typeof options.transform === 'function'
-    ? options.transform(mediaFeature)
+function convertToContainerQuery (mediaFeature, { debug, transform }) {
+  const containerFeature = typeof transform === 'function'
+    ? transform(mediaFeature)
     : mediaFeature;
 
   if (
-    options.debug &&
-      DESCRIPTORS.some(descriptor => containerFeature.includes(descriptor)) &&
-      OPERATORS.some(operator => containerFeature.includes(operator))
+    debug &&
+    DESCRIPTORS.some(descriptor => containerFeature.includes(descriptor)) &&
+    OPERATORS.some(operator => containerFeature.includes(operator))
   ) {
-    console.warn('[mediaToContainerQueryLoader] Unsupported media query', containerFeature);
+    console.warn('[postcssMediaToContainerQueries] Unsupported media query', containerFeature);
   }
 
   return containerFeature;
-};
-
-function loader() {
-  const schema = {
-    title: 'Media to Container Queries Loader options',
-    type: 'object',
-    properties: {
-      debug: {
-        type: 'boolean'
-      },
-      transform: {
-        anyOf: [
-          { type: 'null' },
-          { instanceof: 'Function' }
-        ]
-      }
-    }
-  };
-  const options = this.getOptions(schema);
-
-  const mediaQueryRegex = /@media[^{]*{([\s\S]*?})\s*(\\n)*}/g;
-
-  // Prepend container query to media queries
-  const modifiedSource = source.replace(mediaQueryRegex, (match) => {
-    const root = postcss.parse(match.replaceAll(/(?<!\\)\\[frntv]/g, ''));
-    root.walkAtRules('media', atRule => {
-      if (
-        atRule.params.includes('print') &&
-        (!atRule.params.includes('all') || !atRule.params.includes('screen'))
-      ) {
-        return;
-      }
-
-      // Container query
-      const containerAtRule = atRule.clone({
-        name: 'container',
-        params: convertToContainerQuery(atRule.params)
-          .replaceAll(/(only\s*)?(all|screen|print)(,)?(\s)*(and\s*)?/g, '')
-      });
-
-      // Media query
-      // Only apply when data-breakpoint-preview-mode is not set
-      atRule.walkRules(rule => {
-        const newRule = rule.clone({
-          selectors: rule.selectors.map(selector => {
-            if (selector.startsWith('body')) {
-              return selector.replace('body', ':where(body:not([data-breakpoint-preview-mode]))');
-            }
-
-            return `:where(body:not([data-breakpoint-preview-mode])) ${selector}`;
-          })
-        });
-
-        rule.replaceWith(newRule);
-      });
-
-      root.append(containerAtRule);
-    });
-
-    return root.toString();
-  });
-
-  return modifiedSource;
 };
--- test/index.js
+++ test/index.js
@@ -1,21 +1,22 @@
 const postcss = require('postcss');
 const { equal, deepEqual } = require('node:assert');
 const { postcssCopyViewportToContainerUnits, postcssMediaToContainerQueries } = require('../index.js');
-const opts = { selector: ':where(body[data-breakpoint-preview-mode])' };
+const copyViewportOpts = { selector: ':where(body[data-breakpoint-preview-mode])' };
+const containerQueryOpts = { selector: ':where(body:not([data-breakpoint-preview-mode]))' };
 
 describe('postcss-viewport-to-container-units', () => {
   it('should map `vh` values to `cqh` in a rule that applies only on breakpoint preview ', async () => {
     const input = '.hello { width: 100vh; }';
     const output = '.hello { width: 100vh; }\n:where(body[data-breakpoint-preview-mode]) .hello { width: 100cqh; }';
 
-    await run(postcssCopyViewportToContainerUnits, input, output, opts);
+    await run(postcssCopyViewportToContainerUnits, input, output, copyViewportOpts);
   });
 
   it('should map `vw` values to `cqw` in a rule that applies only on breakpoint preview ', async () => {
     const input = '.hello { width: 100vw; }';
     const output = '.hello { width: 100vw; }\n:where(body[data-breakpoint-preview-mode]) .hello { width: 100cqw; }';
 
-    await run(postcssCopyViewportToContainerUnits, input, output, opts);
+    await run(postcssCopyViewportToContainerUnits, input, output, copyViewportOpts);
   });
 
   it('should map `vh` and `vw` values used in `calc` to `cqh` and `cqw` in a rule that applies only on breakpoint preview', async () => {
@@ -25,7 +26,7 @@ describe('postcss-viewport-to-container-units', () => {
 .hello { height: calc(100vh - 50px); width: calc(100vw - 10px); }
 :where(body[data-breakpoint-preview-mode]) .hello { height: calc(100cqh - 50px); width: calc(100cqw - 10px); }`;
 
-    await run(postcssCopyViewportToContainerUnits, input, output, opts);
+    await run(postcssCopyViewportToContainerUnits, input, output, copyViewportOpts);
   });
 
   it('should add only declarations containing `vh` and `vw` values in a rule that applies only on breakpoint preview', async () => {
@@ -51,12 +52,36 @@ describe('postcss-viewport-to-container-units', () => {
   height: calc(100cqh - 50px);
 }`;
 
-    await run(postcssCopyViewportToContainerUnits, input, output, opts);
+    await run(postcssCopyViewportToContainerUnits, input, output, copyViewportOpts);
+  });
+
+  it.only('should work properly inside media queries', async () => {
+    const input = `
+@media only screen and (width > 600px) and (max-width: 1000px) {
+  .hello {
+    top: 0;
+    width: 100vw;
+    height: calc(100vh - 50px);
+  }
+
+  .goodbye {
+    width: 100%;
+    color: #fff;
+    transform: translateX(20vw);
+  }
+}
+`;
+
+    const output = `
+
+`;
+
+    await run(postcssCopyViewportToContainerUnits, input, output, copyViewportOpts);
   });
 });
 
-describe.only('postcss-media-to-container-queries', () => {
-  it('should convert media queries to container queries', async () => {
+describe('postcss-media-to-container-queries', () => {
+  it('should convert media queries to container queries with one sub rule', async () => {
     const input = `
 @media only screen and (width > 600px) and (max-width: 1000px) {
   .hello {
@@ -70,7 +95,7 @@ describe.only('postcss-media-to-container-queries', () => {
 `;
 
     const output = `
-@media screen and (width > 600px) and (max-width: 1000px) {
+@media only screen and (width > 600px) and (max-width: 1000px) {
   :where(body:not([data-breakpoint-preview-mode])) .hello {
     position: absolute;
     top: 0;
@@ -81,20 +106,121 @@ describe.only('postcss-media-to-container-queries', () => {
 }
 @container (width > 600px) and (max-width: 1000px) {
   .hello {
-    width: 100cqw;
-    height: calc(100cqh - 50px);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: calc(100vh - 50px);
   }
-}`;
+}
+`;
 
     await run(postcssMediaToContainerQueries, input, output, {
       selector: ':where(body:not([data-breakpoint-preview-mode]))'
     });
+  });
+
+  it('should convert media queries to container queries with multiple sub rules', async () => {
+    const input = `
+@media only screen and (width > 600px) and (max-width: 1000px) {
+  .hello {
+    position: absolute;
+    top: 0;
+  }
+
+  .goodbye {
+    width: 100%;
+    color: #fff;
+  }
+}
+`;
+
+    const output = `
+@media only screen and (width > 600px) and (max-width: 1000px) {
+  :where(body:not([data-breakpoint-preview-mode])) .hello {
+    position: absolute;
+    top: 0;
+  }
+
+  :where(body:not([data-breakpoint-preview-mode])) .goodbye {
+    width: 100%;
+    color: #fff;
+  }
+}
+@container (width > 600px) and (max-width: 1000px) {
+  .hello {
+    position: absolute;
+    top: 0;
+  }
+
+  .goodbye {
+    width: 100%;
+    color: # fff;
+  }
+}
+`;
+
+    await run(postcssMediaToContainerQueries, input, output, containerQueryOpts);
+  });
+});
+
+describe('Plugins combination', () => {
+  it('should work properly when both plugins are used in combination', async () => {
+    const input = `
+@media only screen and (width > 600px) and (max-width: 1000px) {
+  .hello {
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    height: calc(100vh - 50px);
+  }
+
+  .goodbye {
+    width: 100%;
+    color: # fff;
+    transform: translateX(20vw);
+  }
+}
+`;
+
+    const output = `
+@media only screen and (width > 600px) and (max-width: 1000px) {
+  .hello {
+    position: absolute;
+    top: 0;
+  }
+
+  .goodbye {
+    width: 100%;
+    color: # fff;
+  }
+}
+`;
+
+    await runBoth(
+      input,
+      output,
+      postcssCopyViewportToContainerUnits,
+      postcssMediaToContainerQueries,
+      copyViewportOpts,
+      containerQueryOpts
+    );
   });
 });
 
 // From https://github.com/postcss/postcss-plugin-boilerplate/blob/main/template/index.test.t.js
 async function run(plugin, input, output, opts = {}) {
   const result = await postcss([ plugin(opts) ]).process(input, { from: undefined });
+
+  console.log('result.css', result.css);
+  equal(result.css, output);
+  deepEqual(result.warnings(), []);
+}
+
+async function runBoth(input, output, plugin1, plugin2, opts1, opts2) {
+  const firstRes = await postcss([ plugin1(opts1) ]).process(input, { from: undefined });
+  const result = await postcss([ plugin2(opts2) ])
+    .process(firstRes.css, { from: undefined });
 
   equal(result.css, output);
   deepEqual(result.warnings(), []);
