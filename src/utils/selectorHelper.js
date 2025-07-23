a86e541aa7e5632462096e7d0e046e634891b89a
E.T <8301962+ETLaurent@users.noreply.github.com>
2025-07-23 18:39:19 +0200

    re-add `:where()` pseudo-class to fix specificity issues (#8)
    * re-add `:where()` pseudo-class to fix specificity issues only when we add selectors
    * update mocha tests
    
    ---------
    
    Co-authored-by: Etienne Laurent <git.sabbath464@passmail.net>
    Co-authored-by: Jed <vjeudy@protonmail.com>

--- src/utils/selectorHelper.js
+++ src/utils/selectorHelper.js
@@ -2,6 +2,7 @@
 const createSelectorHelper = ({ modifierAttr }) => {
   const bodyRegex = /^body|^html.*\s+body|^html.*\s*>\s*body/;
   const tagRegex = /^\.|^#|^\[|^:/;
+  const wrapInWhere = (selector) => `:where(${selector})`;
 
   const addTargetToSelectors = (
     selector,
@@ -11,11 +12,12 @@ const createSelectorHelper = ({ modifierAttr }) => {
       .split(',')
       .reduce((acc, part) => {
         const trimmed = part.trim();
-        if (!trimmed.match(bodyRegex)) {
-          acc.push(`${target} ${trimmed}`);
+        const isBodySelector = trimmed.match(bodyRegex);
+        if (!isBodySelector) {
+          acc.push(`${wrapInWhere(target)} ${trimmed}`);
         }
 
-        const bodyLevelSelector = getBodyLevelSelector(trimmed, target);
+        const bodyLevelSelector = getBodyLevelSelector(trimmed, target, isBodySelector);
         if (bodyLevelSelector) {
           acc.push(bodyLevelSelector);
         }
@@ -54,8 +56,8 @@ const createSelectorHelper = ({ modifierAttr }) => {
     return updatedSelector.join(',\n  ');
   };
 
-  const getBodyLevelSelector = (selector, target) => {
-    if (selector.match(bodyRegex)) {
+  const getBodyLevelSelector = (selector, target, isBodySelector) => {
+    if (isBodySelector) {
       selector = selector.replace(bodyRegex, '');
 
       // Selector is a body without identifiers, we put style in the body directly
@@ -68,7 +70,8 @@ const createSelectorHelper = ({ modifierAttr }) => {
     // in case the body has this identifier
     const noTagSelector = selector.match(tagRegex);
     if (noTagSelector) {
-      return `${target}${selector}`;
+      const targetSelector = isBodySelector ? target : wrapInWhere(target);
+      return `${targetSelector}${selector}`;
     }
 
     return null;
